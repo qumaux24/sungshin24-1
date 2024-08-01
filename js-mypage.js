80cb664251d171a3f78ece1449e92bdc748a2d0b
yeeun <yyeeun2107@gmail.com>
2024-08-01 17:02:24 +0900

    mypage

---
+++ js-mypage.js
@@ -0,0 +1,20 @@
+function loadContent(url) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    document.getElementById('content').innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var radios = document.querySelectorAll('.dropdown-mypage input[type="radio"]');
+            radios.forEach(function(radio) {
+                radio.addEventListener('change', function(event) {
+                    var url = event.target.value;
+                    loadContent(url);
+                });
+            });
+        });
