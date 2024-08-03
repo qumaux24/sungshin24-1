fba9ba11b2b37a743e4bad553c5eab751f878d55
sujin <20241005@sungshin.ac.kr>
2024-08-04 03:12:38 +0900

    로그인, 회원가입 구현 O, 로그인 전과 후 구분 X

--- study/post/urls.py
+++ study/post/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from accounts.urls import login, signup
 
 urlpatterns = [
     path('', views.list, name='list'),
+    path('signup/', signup, name='signup'),
+    path('login', login, name='login')
 ]
