868e6c6e8a85320d203d03414127a92ad4dc4705
Son <suruna1026@gmail.com>
2024-08-07 13:05:26 +0900

    유저디테일, 패스키 모델/폼 추가

---
+++ study/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import User_detail, Pass_keyword
+
+class UserdetailForm(forms.ModelForm):
+
+    class Meta:
+        model = User_detail
+        fields = ['allergy']
+    
+class PasskeywordForm(forms.ModelForm):
+    
+    class Meta:
+        model = Pass_keyword
+        fields = ['pass_keyword']
--- study/accounts/models.py
+++ study/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class User_detail(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    allergy = models.TextField()
+    
+    def __str__(self):
+        # return self.user
+        return self.user.username
+    
+class Pass_keyword(models.Model):
+    user=models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pass_keyword = models.CharField(max_length=100)
+    def __str__(self):
+        # return self.user
+        return self.user.username
